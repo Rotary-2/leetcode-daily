062498e2ae524f02f745f65fd29dd9a27cb25285
Rotary-2 <yjcdghknb@163.com>
2026-01-11 09:45:31 +0800

    2026-1-11

---
+++ 66.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int i = digits.size() - 1;
+
+        while (digits[i] == 9) {
+            digits[i] = 0;
+            if (i == 0) {
+                digits.insert(digits.begin(), 1);
+                return digits;
+            } else (i--);
+        }
+        if (digits[i] != 9) {
+            digits[i]++;
+        }
+
+        return digits;
+    }
+};
