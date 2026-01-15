5f44a2e10a38a4e7e7f887f45346e04c6c9e75dd
Rotary-2 <yjcdghknb@163.com>
2026-01-15 08:17:02 +0800

    2026-1-15

---
+++ 2.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* l3 = new ListNode(0);
+        ListNode* p = l3;
+        int CF = 0;
+
+        while (l1 || l2 || CF) {
+            int sum = CF;
+
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            CF = sum / 10;
+            p->next = new ListNode(sum % 10);
+            p = p->next;
+        }
+        return l3->next;
+    }
+};
